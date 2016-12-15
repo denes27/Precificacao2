d54ed1d2c61d09ebe55df1a3ddf98dbb4a415ab0
wtkimura <willkimura@hotmail.com>
2016-12-15 02:26:10 -0200

    Add files via upload
    Ele ainda esta incompleto... Vou tentar concluir amanha...

---
+++ app/src/main/java/com/example/denesleal/precificacao/ListaOrcamento.java
@@ -0,0 +1,25 @@
+package com.example.denesleal.precificacao;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by User on 15/12/2016.
+ */
+
+public class ListaOrcamento extends Activity {
+    MeuBD meuBD = new MeuBD(this, MeuBD.DB_NAME, null, MeuBD.DB_VERSION);
+    private ProdutosDAO produtosDAO = new ProdutosDAO(meuBD);
+    ArrayList<Produtos> lista = (ArrayList<Produtos>)produtosDAO.read(null);
+    private ListView products;
+
+    @Override
+    protected void OnCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculo_despesas);
+    }
+}
